335389006b65a7b19a497175e0722095d8cff1d6
aCLr <a.ch.clr@gmail.com>
2021-02-06 00:06:14 +0300

    more

--- template/rust-tdlib/client/client.rs
+++ template/rust-tdlib/client/client.rs
@@ -6,7 +6,6 @@ use crate::{
     types::*,
 };
 use tokio::sync::mpsc;
-use tokio::task::JoinHandle;
 
 #[doc(hidden)]
 pub trait TdLibClient {
@@ -67,10 +66,8 @@ where
     S: TdLibClient,
 {
     raw_api: S,
-    client_id: i32,
+    client_id: Option<i32>,
     updates_sender: Option<mpsc::Sender<Box<TdType>>>,
-    auth_state_sender: mpsc::Sender<ClientState>,
-    auth_state_receiver: Option<mpsc::Receiver<ClientState>>,
     tdlib_parameters: TdlibParameters,
 }
 
@@ -78,19 +75,30 @@ impl<S> Client<S>
 where
     S: TdLibClient,
 {
-    pub fn client_id(&self) -> i32 {
-        self.client_id
-    }
+
     pub fn tdlib_parameters(&self) -> &TdlibParameters {
         &self.tdlib_parameters
     }
 
-    pub(crate) fn updates_sender(&self) -> &Option<mpsc::Sender<Box<TdType>>> {
-        &self.updates_sender
+    fn get_client_id(&self) -> RTDResult<i32> {
+      match self.client_id {
+        Some(client_id) => Ok(client_id),
+        None => Err(RTDError::BadRequest("client not authorized yet"))
+      }
     }
 
-    pub(crate) fn auth_sender(&self) -> &mpsc::Sender<ClientState> {
-        &self.auth_state_sender
+    pub(crate) fn set_client_id(&mut self, client_id: i32) -> RTDResult<()> {
+        match self.client_id {
+            Some(_) => Err(RTDError::BadRequest("client already authorized")),
+            None => {
+                self.client_id = Some(client_id);
+                Ok(())
+            }
+        }
+    }
+
+    pub(crate) fn updates_sender(&self) -> &Option<mpsc::Sender<Box<TdType>>> {
+        &self.updates_sender
     }
 }
 
@@ -118,7 +126,6 @@ impl<R> ClientBuilder<R>
 where
     R: TdLibClient,
 {
-    /// If you want to receive real-time updates (new messages, calls, etc.) you have to receive them with tokio::mpsc::Receiver<TdType>
     pub fn with_updates_sender(mut self, updates_sender: mpsc::Sender<Box<TdType>>) -> Self {
         self.updates_sender = Some(updates_sender);
         self
@@ -130,7 +137,7 @@ where
         self
     }
 
-    pub fn with_tdjson<T: TdLibClient>(mut self, tdjson: T) -> ClientBuilder<T> {
+    pub fn with_tdjson<T: TdLibClient>(self, tdjson: T) -> ClientBuilder<T> {
         ClientBuilder {
             tdjson,
             updates_sender: self.updates_sender,
@@ -167,41 +174,14 @@ where
         updates_sender: Option<mpsc::Sender<Box<TdType>>>,
         tdlib_parameters: TdlibParameters,
     ) -> Self {
-        let client_id = tdjson::new_client();
-        let (auth_state_sender, auth_state_receiver) = mpsc::channel(10);
         Self {
             raw_api,
-            client_id,
-            auth_state_receiver: Some(auth_state_receiver),
-            auth_state_sender,
             updates_sender,
             tdlib_parameters,
+            client_id: None,
         }
     }
 
-    pub async fn wait_for_auth(&self) -> RTDResult<JoinHandle<ClientState>> {
-        let mut recv = match &self.auth_state_receiver {
-            None => return Err(RTDError::BadRequest("client already initialized")),
-            Some(recv) => {
-                recv
-            }
-        };
-        if let Some(msg) = recv.recv().await {
-            match msg {
-                ClientState::Closed => return Ok(tokio::spawn(async { ClientState::Closed })),
-                ClientState::Error(e) => return Err(RTDError::TdlibError(e)),
-                ClientState::Opened => {}
-            }
-        }
-        Ok(tokio::spawn(async move {
-             match recv.recv().await {
-                Some(ClientState::Opened) => ClientState::Error("received Opened state again".to_string()),
-                Some(state) => state,
-                None => ClientState::Error("auth state channel closed".to_string())
-            }
-        }))
-
-    }
 
 {% for token in tokens %}{% if token.type_ == 'Function' %}
   // {{ token.description }}
@@ -209,7 +189,7 @@ where
     let extra = {{token.name | to_snake }}.as_ref().extra()
       .ok_or(RTDError::Internal("invalid tdlib response type, not have `extra` field"))?;
     let signal = OBSERVER.subscribe(&extra);
-    self.raw_api.send(self.client_id, {{token.name | to_snake }}.as_ref())?;
+    self.raw_api.send(self.get_client_id()?, {{token.name | to_snake }}.as_ref())?;
     let received = signal.await;
     OBSERVER.unsubscribe(&extra);
     match received {
