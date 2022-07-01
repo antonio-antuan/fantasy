f9ecec0da5ac446b8d458dc702dcaef01b11c977
aCLr <a.ch.clr@gmail.com>
2022-07-01 22:26:43 +0300

    minifixes for testing and linter

--- template/rust-tdlib/client/api.rs
+++ template/rust-tdlib/client/api.rs
@@ -1,17 +1,10 @@
-use super::{
-  observer::OBSERVER,
-  tdlib_client::TdLibClient,
-};
+use super::Client;
+use super::tdlib_client::TdLibClient;
 use crate::{
-  errors::{RTDError, RTDResult},
+  errors::RTDResult,
   types::*,
 };
-use super::Client;
 
-const CLOSED_RECEIVER_ERROR: RTDError = RTDError::Internal("receiver already closed");
-const INVALID_RESPONSE_ERROR: RTDError = RTDError::Internal("receive invalid response");
-const NO_EXTRA: RTDError =
-  RTDError::Internal("invalid tdlib response type, not have `extra` field");
 
 impl<R> Client<R>
 where
@@ -20,22 +13,7 @@ where
 {% for token in tokens %}{% if token.type_ == 'Function' %}
   // {{ token.description }}
   pub async fn {{token.name | to_snake}}<C: AsRef<{{token.name | to_camel}}>>(&self, {{token.name | to_snake}}: C) -> RTDResult<{{token.blood | to_camel}}> {
-    let extra = {{token.name | to_snake }}.as_ref().extra()
-      .ok_or(NO_EXTRA)?;
-    let signal = OBSERVER.subscribe(&extra);
-    self.tdlib_client.send(self.get_client_id()?, {{token.name | to_snake }}.as_ref())?;
-    let received = signal.await;
-    OBSERVER.unsubscribe(&extra);
-    match received {
-      Err(_) => {Err(CLOSED_RECEIVER_ERROR)}
-      Ok(v) => match serde_json::from_value::<{{token.blood | to_camel}}>(v) {
-        Ok(v) => { Ok(v) }
-        Err(e) => {
-          log::error!("response serialization error: {:?}", e);
-          Err(INVALID_RESPONSE_ERROR)
-        }
-      }
-    }
+    self.make_request({{token.name | to_snake}}).await
   }
   {% endif %}{% endfor %}
 }
